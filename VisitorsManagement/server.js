fedb2979ece188c60a3c3810f8597d835154b043
assaduzzaman <zaman_ael@hotmail.com>
2022-08-29 17:54:33 +0600

    asdf

--- VisitorsManagement/server.js
+++ VisitorsManagement/server.js
@@ -28,6 +28,30 @@ app.get('/locations', async (req, res) => {
     }); 
 });
 
+app.get('/token', async (req, res) => {
+    var request = require('request');
+    var options = {
+        'method': 'POST',
+        'url': 'https://api.envoy.com/oauth2/token',
+        'headers': {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'x-api-key': 'ODMzMjAzZjItMjUxMy0xMWVkLWFhZGUtYzdhOGUzYjc4NDk5OmJjMjgwZjExNWMwYzA2OTY5ZjY4MWQ5YmJkM2UyOTkzMjAxZGVmZWRhZjBhZmVmOTdiMDc2MTAyODNhNWEwYzc3N2EwMTkyZjg4NmQ3YWFkMGY4OTQ4OTU1ZDk3Yjc5MzUzOTM0ZDZjNjU4YzQ3ZTBhNDhlODRlMDVlNzgwMDA3',
+            'Authorization': 'Basic ODMzMjAzZjItMjUxMy0xMWVkLWFhZGUtYzdhOGUzYjc4NDk5OmJjMjgwZjExNWMwYzA2OTY5ZjY4MWQ5YmJkM2UyOTkzMjAxZGVmZWRhZjBhZmVmOTdiMDc2MTAyODNhNWEwYzc3N2EwMTkyZjg4NmQ3YWFkMGY4OTQ4OTU1ZDk3Yjc5MzUzOTM0ZDZjNjU4YzQ3ZTBhNDhlODRlMDVlNzgwMDA3',
+            'Cookie': '_EnvoyWeb_session=TkxNMkZlMXdUKzRkeTNVaTZDRE5kS3dJYnNnV3FZcXVyZDQ1b3pIajNSNHorNnovempNeTNYcytWejlMdHVSTjMvdlZ3dXd5UUdvU0dCcTMwU2tEbTFPcmFBUW8zQmYycHdzWWFGRDdESkdmWlRWNTAybDVaOW9admlXanVZRHJFWW81UDM0LzI4dXFoUU9EY0ZxaE1BTW5VZVRaaUF6Sm15YVlaMUFwVkJkV0YzZTBEcTZIeUZGUVN6QzMwVktzLS1VYVFVNWltTVR6YjdkOE1EVUZOOUZRPT0%3D--7d0a551e06a3bd4e301352c3122542caff3469be; access_token=eyJhbGciOiJFUzM4NCJ9.eyJpYXQiOjE2NjE3NzM2NjAsImlzcyI6ImlkLmVudm95LmNvbSIsImp0aSI6Ijk1YzMyYjMwLTgyOTQtNDc5Mi05ODQ4LTNmYzJjMzQ5NjNkZSIsImV4cCI6MTY2MTg2MDA2MCwic3ViIjoiVXNlcjoyNDcyMTMxIiwiYXVkIjoiODMzMjAzZjItMjUxMy0xMWVkLWFhZGUtYzdhOGUzYjc4NDk5IiwidHlwIjoiYWNjZXNzIiwic2NvcGVzIjpbInRva2VuLnJlZnJlc2giLCJsb2NhdGlvbnMucmVhZCJdLCJtZXRhIjp7ImNvbXBhbnlfaWQiOiIxMzU0MzAiLCJ0aHJvd19hd2F5X3Jvb21zX2FkbWluX3VzZXJfZmxhZyI6dHJ1ZSwiZXh0ZXJuYWxfZGV2IjoidHJ1ZSJ9fQ.S44diM1hk0T2qJyjyT6Nl6NVkwxG4-uJ9wmDkQzZ_XhjEDyPHDu1LNlukbh4vDX-ZWbltV-P9IL04d6jTHpsADs3t1a59v2FpOWuGitCGXQ7-1x6VWb5EyNVWtmWM-Dz; company_id=135430; csrf_token=a8b3afe5b68c023ff2af488775e2d350bff9c0f1aa34393d0e3d6d00449ad5bd; expires_at=1661860060; expires_in=86400; refresh_token=t8hn4EZbzCGk5NAsDCvUjDH1; ajs_anonymous_id=%2249e8e16c-200d-4e31-bd27-42e0427b6b1e%22; landing_url=https%3A%2F%2Fapi.envoy.com%2Fa%2Fauth%2Fv0%2Ftoken; referring_url='
+        },
+        form: {
+            'username': 'titucse700@gmail.com',
+            'password': 'Admin#123456',
+            'scope': 'token.refresh,locations.read',
+            'grant_type': 'password'
+        }
+    };
+    request(options, function (error, response) {
+        if (error) throw new Error(error);
+        res.send(response.body);
+    });
+});
+
 app.post('/hello-options', (req, res) => {
     res.send([
         {
