e223cb12514ecbc7ff841f02c424ffbc4ac3e9b9
assaduzzaman <zaman_ael@hotmail.com>
2022-08-31 12:01:26 +0600

    asdfsdf

---
+++ VisitorManagement/Account.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VisitorManagement
+{
+    public class Account
+    {
+        public string Email { get; internal set; }
+        public bool Active { get; internal set; }
+        public DateTime CreatedDate { get; internal set; }
+        public List<string> Roles { get; internal set; }
+    }
+}
