13c6a1d8e69997c6b093fbc3635779ed5a89f577
assaduzzaman <zaman_ael@hotmail.com>
2022-09-12 20:26:25 +0600

    asdf sadf sdf

--- EnvoyNetFrameworkSdk/CA4KApi.cs
+++ EnvoyNetFrameworkSdk/CA4KApi.cs
@@ -98,6 +98,8 @@ namespace EnvoyNetFrameworkSdk
             ushort operation = 0; //insert;
             bool badgeExists = caAccess.BadgeExists((long)badge, facility, pivi);
 
+            CreateTimeSchedule($"Time schedule {badge}", 2);
+
             if (badgeExists)
             {
                 operation = 2; // update
@@ -140,5 +142,20 @@ namespace EnvoyNetFrameworkSdk
                 caAccess.BadgeOperation(facility, badge, firstName, lastName, mi, new ushort[] { accessGroup }, operation);
             }
         }
+
+        public bool CreateTimeSchedule(string name, int no)
+        {
+            bool result = caAccess.TimeScheduleOperation(name, no, 0) == 0;
+            DateTime now = DateTime.Now;
+
+            return result && CreateTimeScheduleTimeBlock(no, 0, (int)now.DayOfWeek, (int)now.DayOfWeek, now, now.AddSeconds(60 * 2));
+        }
+
+        public bool CreateTimeScheduleTimeBlock(int no, int index, int dayFrom, int dayTo, DateTime timeFrom, DateTime timeTo) =>
+           caAccess.TimeScheduleTimeBlockOperation(no, index, dayFrom, dayTo, timeFrom, timeTo, 0) == 0;
+
+        public bool DeleteTimeSchedule(string name, int no) =>
+            caAccess.TimeScheduleOperation(name, no, 1) == 0;
     }
 }
+
